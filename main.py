99090940f07756b78481b38e59d9310c59137a24
Duwareon <Duwareon@gmail.com>
2021-03-27 20:55:27 -0400

    Added ping validation checking and added errors

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 bot = commands.Bot(command_prefix='!')
+
+incorrectPingErrMsg = "ping a user ya donkass"
 
 
 @bot.event
@@ -19,52 +22,70 @@ async def on_ready():
 
 @bot.command()
 async def hug(ctx, arg1, gif: typing.Optional[str] = "mf"):
-    await ctx.send("{0.author} hugged {1}".format(ctx, arg1))
+    if arg1.startswith("<@!") and arg1.endswith(">"):
+        await ctx.send("<@!{0}> hugged {1}".format(ctx.author.id, arg1))
 
-    if gif == "mm":
-        await ctx.send(random.choice(gifs.hugmm))
+        if gif == "mm":
+            await ctx.send(random.choice(gifs.hugmm))
 
-    elif gif == "mf":
-        await ctx.send(random.choice(gifs.hugmf))
+        elif gif == "mf":
+            await ctx.send(random.choice(gifs.hugmf))
 
-    elif gif == "ff":
-        await ctx.send(random.choice(gifs.hugff))
+        elif gif == "ff":
+            await ctx.send(random.choice(gifs.hugff))
 
+        else:
+            await ctx.send(random.choice(gifs.hugmf))
+    elif "<@!" in ctx.message.content and ">" in ctx.message.content:
+        await ctx.send("the command format is \"{}hug [user] [mm/mf/ff]\"".format(bot.command_prefix))
     else:
-        await ctx.send(random.choice(gifs.hugmf))
+        await ctx.send(incorrectPingErrMsg)
 
 
-@bot.command()
+@ bot.command()
 async def kiss(ctx, arg1, gif: typing.Optional[str] = "mf"):
-    await ctx.send("{0.author} kissed {1}".format(ctx, arg1))
+    if arg1.startswith("<@!") and arg1.endswith(">"):
+        await ctx.send("<@!{0}> kissed {1}".format(ctx.author.id, arg1))
 
-    if gif == "mm":
-        await ctx.send(random.choice(gifs.kissmm))
+        if gif == "mm":
+            await ctx.send(random.choice(gifs.kissmm))
 
-    elif gif == "mf":
-        await ctx.send(random.choice(gifs.kissmf))
+        elif gif == "mf":
+            await ctx.send(random.choice(gifs.kissmf))
 
-    elif gif == "ff":
-        await ctx.send(random.choice(gifs.kissff))
+        elif gif == "ff":
+            await ctx.send(random.choice(gifs.kissff))
+
+        else:
+            await ctx.send(random.choice(gifs.kissmf))
+
+    elif "<@!" in ctx.message.content and ">" in ctx.message.content:
+        await ctx.send("the command format is \"{}kiss [user] [mm/mf/ff]\"".format(bot.command_prefix))
 
     else:
-        await ctx.send(random.choice(gifs.kissmf))
+        await ctx.send(incorrectPingErrMsg)
 
 
-@bot.command()
+@ bot.command()
 async def cuddle(ctx, arg1, gif: typing.Optional[str] = "mf"):
-    await ctx.send("{0.author} cuddled {1}".format(ctx, arg1))
+    if arg1.startswith("<@!") and arg1.endswith(">"):
+        await ctx.send("<@!{0}> cuddled {1}".format(ctx.author.id, arg1))
 
-    if gif == "mm":
-        await ctx.send(random.choice(gifs.cuddlemm))
+        if gif == "mm":
+            await ctx.send(random.choice(gifs.cuddlemm))
 
-    elif gif == "mf":
-        await ctx.send(random.choice(gifs.cuddlemf))
+        elif gif == "mf":
+            await ctx.send(random.choice(gifs.cuddlemf))
 
-    elif gif == "ff":
-        await ctx.send(random.choice(gifs.cuddleff))
+        elif gif == "ff":
+            await ctx.send(random.choice(gifs.cuddleff))
 
+        else:
+            await ctx.send(random.choice(gifs.cuddlemf))
+
+    elif "<@!" in ctx.message.content and ">" in ctx.message.content:
+        await ctx.send("the command format is \"{}cuddle [user] [mm/mf/ff]\"".format(bot.command_prefix))
     else:
-        await ctx.send(random.choice(gifs.cuddlemf))
+        await ctx.send(incorrectPingErrMsg)
 
 bot.run(os.getenv("TOKEN"))
